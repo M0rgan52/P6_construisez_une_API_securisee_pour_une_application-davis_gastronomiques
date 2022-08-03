fe783c1693d15a37eb2dcda455db6cb0a1bd3aaf
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-03 12:22:40 +0200

    commit -m "stabilisation server.js"

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,23 @@
 const express = require('express');
 const app = express();
 
+app.use ((req, res, next) => {
+    console.log('Requête reçue');
+    next();
+});
+
+app.use ((req, res, next) => {
+    res.status(201);
+    next()
+});
 // Création de la réponse de base de la requête
-app.use ((req, res) => {
+app.use ((req, res, next) => {
     res.json({message : 'Le server est en fonctionnement'});
+    next();
+});
+
+app.use ((req, res) => {
+    console.log('Réponse envoyée avec succès !');
 });
 
 // Export de la constante
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,50 @@
 const http = require('http');
 const app = require('./app');
 
-// Appel de la fonction app et paramétrage du port
-app.set('port', process.env.PORT || 3000);
+// renvoie un port valide sous forme d'un numéro ou d'une chaine
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+//Recherche des erreurs  
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port:' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+//Informations sur le port utilisé
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen(port);
