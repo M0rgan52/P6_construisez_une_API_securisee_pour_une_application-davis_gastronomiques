ad1901ee0964c29448ca017b0af27c8932f27101
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-05 10:04:00 +0200

    commit -m "CORS"

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const app = express();
 
+// Création des headers pour les erreurs CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();s
+})
+
 app.use ((req, res, next) => {
     console.log('Requête reçue');
     next();
