b97711ec0a0dcd5decc6280f44ad9ada7eb93e51
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-06 11:20:07 +0200

    commit -m "Initialisation schema sauce"

---
+++ backend/models/sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    userID: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: false },
+    dislikes: { type: Number, required: false },
+    userLiked: { type: [String<userID>], required: false },
+    userDisliked: { type: [String<userID>], required: false },
+});
+
+module.exports = mongoose.model('sauce', sauceSchema);
