2e570e6c373fdffda341387cddcdccd9698a6d89
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-11 17:14:18 +0200

    commit -m "routes sauces"

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,22 @@
 // Création des constantes
 const express = require('express');
 const mongoose = require('mongoose');
-// const sauceRoutes = require('./routes/sauce');
-const userRoutes = require('./routes/user');
-const app = express();
 
-// Gestion des requêtes Express en JSON
-app.use(express.json()); 
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 // Connexion API MongoDB
 mongoose.connect('mongodb+srv://Morgan:PiiquanteP6@cluster0.uodwo5p.mongodb.net/?retryWrites=true&w=majority',
 { useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+// Gestion des requêtes Express en JSON
+app.use(express.json()); 
 
 
 // Création des headers pour les erreurs CORS
@@ -26,28 +28,8 @@ app.use((req, res, next) => {
 })
 
 // Enregistrement des routeurs 
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
-
-
-app.use ((req, res, next) => {
-    console.log('Requête reçue');
-    next();
-});
-
-app.use ((req, res, next) => {
-    res.status(201);
-    next()
-});
-
-app.use ((req, res, next) => {
-    res.json({message : 'Le server est en fonctionnement'});
-    next();
-});
-
-app.use ((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-});
-
 
 // Export de la constante
 module.exports = app;
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+const Sauce = require('../models/sauce');
+
 const multer = require('../middleware/multer-config');
 
+router.post('/', (req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...req.body
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+router.put('/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+router.delete('/:id', (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+});
+
+router.get('/', (req, res, next) => {
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
 
 
 module.exports = router;
