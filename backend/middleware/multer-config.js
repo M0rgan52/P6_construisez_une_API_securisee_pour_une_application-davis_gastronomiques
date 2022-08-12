32dccf6c320ded8a9368e0131eeb497727d9cc2e
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-12 11:33:32 +0200

    commit -m "Modif middlew multer"

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -2,7 +2,7 @@
 const multer = require('multer');
 const MIME_TYPES = {
     'image/jpg': 'jpg',
-    'image/jpeg': 'jpeg',
+    'image/jpeg': 'jpg',
     'image/png': 'png'
 };
 
@@ -24,4 +24,4 @@ const storage = multer.diskStorage({
     }
 });
 
-module.exports = multer({ storage: storage }).single('image');
+module.exports = multer({ storage }).single('image');
