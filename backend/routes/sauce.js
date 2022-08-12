0966312cada2932dea50d37a9f23c8284f9e647a
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-12 11:42:47 +0200

    commit -m "ajout multer routes"

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
 
-const multer = require('../middleware/multer-config');
 
-router.post('/', auth, sauceCtrl.createSauce);
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.get('/', auth, sauceCtrl.getAllSauce);
+
 
 
 module.exports = router;
