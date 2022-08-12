932569f99a77342278e53a2dac70d3f0ba3181ba
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-12 14:53:17 +0200

    commit -m "modif schema sauces"

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    userID: { type: String, required: true },
+    userId: { type: String, required: true },
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, required: false },
-    dislikes: { type: Number, required: false },
-    userLiked: { type: [String], required: false },
-    userDisliked: { type: [String], required: false },
+    likes: { type: Number },
+    dislikes: { type: Number },
+    userLiked: { type: [String] },
+    userDisliked: { type: [String] },
 });
 
-module.exports = mongoose.model('sauce', sauceSchema);
+module.exports = mongoose.model('Sauce', sauceSchema);
