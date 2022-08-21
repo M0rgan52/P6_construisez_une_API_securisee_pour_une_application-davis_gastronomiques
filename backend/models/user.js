4339a257630c5093ec343c74155b2a85a37858f4
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-21 10:07:31 +0200

    commit -m "controle email pour inscription"

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,7 @@
 // Création des constantes
 const mongoose = require('mongoose');
+require('mongoose-type-email');
+
 const uniqueValidator = require('mongoose-unique-validator');
 
 /* Création du schéma utilisateurs 
@@ -9,7 +11,7 @@ Ajout du plugin Mongoose Unique Validator pour vérifier
 que le mot de passe soit bien unique avant l'exportation comme modèle
 */
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"] },
     password: { type: String, required: true }
 });
 
