c065f969f5fab3d3cf138578ce365a598887096f
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-22 11:46:45 +0200

    commit -m "Ajout commentaire routes sauce"

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,3 +1,4 @@
+// Création des constantes
 const express = require('express');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
@@ -5,6 +6,11 @@ const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
 
+/*Création des routes prévue par le frontend
+1- Controle du token pour les authorisation
+2- Multer pour les images
+3- Redirection vers la fonction souhaité
+*/ 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/', auth, sauceCtrl.getAllSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
